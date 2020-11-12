1d137278bbdafc2aca729cdf0dc15b9335f30d8f
geumseongYoon <ygs9405@naver.com>
2020-11-13 05:19:22 +0900

    stack & queue 3 - truck on the bridge(fail)

---
+++ python/1113.py
@@ -0,0 +1,46 @@
+def solution(bridge_length, weight, truck_weights):
+    answer = 0
+    currentQueue = list()
+    currentValue = list()
+
+    while(len(truck_weights) or len(currentQueue)):
+        popCheck = 0
+        currentSum = 0
+
+        for i in range(len(currentQueue)):
+            if currentQueue[0] == bridge_length-1:
+                currentQueue.pop(0)
+                currentValue.pop(0)
+                popCheck = 1
+
+            else:
+                currentQueue[i-popCheck] += 1
+                currentSum += currentValue[i-popCheck]
+
+        if len(truck_weights) > 0:
+            if (truck_weights[0] + currentSum) <= weight:
+                currentQueue.append(0)
+                currentValue.append(truck_weights.pop(0))
+
+        # if len(truck_weights) or len(currentQueue):
+        #     answer += 1
+        answer += 1
+        # print("currentQueue")
+        # print(currentQueue)
+        # print("currentValue")
+        # print(currentValue)
+    return answer
+
+# bridge_length = 2
+# weight = 10
+# truck_weights = [7, 4, 5, 6]
+
+# bridge_length = 100
+# weight = 100
+# truck_weights = [10]
+
+bridge_length = 100
+weight = 100
+truck_weights = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]
+
+print(solution(bridge_length, weight, truck_weights))
