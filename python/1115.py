ced8e8d3db80cb8f1ea7883a4786ee1ca2a12e84
geumseongyoon <ygs9405@naver.com>
2020-11-16 04:10:47 +0900

    heap 01 - scoville (temp)

--- python/1115.py
+++ python/1115.py
@@ -1,12 +1,21 @@
-answer = 0
+
 def solution(scoville, K):
-    scovilleSorted = sorted(scoville)
-    # if len(scoville) <= 1:
-    #     return -1
-    # for i in range(len(scoville)):
-    #     if scoville[i] < K:
-            
-    return scovilleSorted
+    answer = 0
+    while(1):
+        scovilleSorted = sorted(scoville)
+        if len(scovilleSorted) <= 1:
+            return -1
+        elif scovilleSorted[0] < K:
+            firstValue = scovilleSorted.pop(0)
+            secondValue = scovilleSorted.pop(0)
+            calResult = firstValue + 2*secondValue
+            scovilleSorted.append(calResult)
+            scoville = scovilleSorted
+            print(scoville)
+
+        else:
+            return answer
+        answer += 1
 
 scoville = [1, 2, 3, 9, 10, 12]
 K = 7
