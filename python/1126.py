93f71c03b9aa197c683c33ab9ca9ac6db46f470d
geumseongYoon <ygs9405@naver.com>
2020-11-26 21:13:54 +0900

    Heap practice

---
+++ python/1126.py
@@ -0,0 +1,22 @@
+def solution(scoville, K):
+    answer = 0
+    scovilleSorted = sorted(scoville)
+    while(1):
+        
+        if len(scovilleSorted) <= 1:
+            return -1
+        elif scovilleSorted[0] < K:
+            firstValue = scovilleSorted.pop(0)
+            secondValue = scovilleSorted.pop(0)
+            calResult = firstValue + 2*secondValue
+            scovilleSorted.append(calResult)
+            scoville = scovilleSorted
+            print(scoville)
+
+        else:
+            return answer
+        answer += 1
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+print(solution(scoville, K))
