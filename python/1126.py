4b7ec250994adf27e820b251e4e48613c4ccca1c
geumseongYoon <ygs9405@naver.com>
2020-11-27 19:24:05 +0900

    Heap problem 01 - wrong answer

--- python/1126.py
+++ python/1126.py
@@ -1,22 +1,33 @@
 def solution(scoville, K):
     answer = 0
+    flag = 0
+    popCount = 0
     scovilleSorted = sorted(scoville)
+    for i in range(len(scovilleSorted)):
+        if scovilleSorted[i-popCount] > K:
+            if flag >= 1:
+                scovilleSorted.pop(i-popCount)
+                popCount += 1
+                continue
+            else:
+                flag += 1
+
     while(1):
-        
         if len(scovilleSorted) <= 1:
-            return -1
-        elif scovilleSorted[0] < K:
-            firstValue = scovilleSorted.pop(0)
-            secondValue = scovilleSorted.pop(0)
-            calResult = firstValue + 2*secondValue
-            scovilleSorted.append(calResult)
-            scoville = scovilleSorted
-            print(scoville)
+            break
+        for j in range(len(scovilleSorted)):
+            if scovilleSorted[j] < K:
+                firstValue = scovilleSorted.pop(0)
+                secondValue = scovilleSorted.pop(0)
+                calResult = firstValue + 2*secondValue
+                if calResult < K:
+                    scovilleSorted.append(calResult)
+                    scovilleSorted = sorted(scovilleSorted)
+                break
 
-        else:
-            return answer
         answer += 1
+    return answer
 
-scoville = [1, 2, 3, 9, 10, 12]
-K = 7
-print(solution(scoville, K))
+# scoville = [1, 2, 3, 9, 10, 12]
+# K = 7
+# print(solution(scoville, K))
